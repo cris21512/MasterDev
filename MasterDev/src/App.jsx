126ec4cd4c9cc09489b298abf6994b2805aec943
Master21 <mastercrack2112.5@gmail.com>
2025-09-08 09:40:58 -0600

    config proyect

---
+++ MasterDev/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <h1> Hello everyone</h1>
+    <a href="https://youtube.com">Click me please</a>
+    <p>Hi there! ¿how are you today?</p>
+    </>
+  )
+}
+
+export default App
