d712fe6828b0e06a69b02e9f86f4ed9e501f16bf
Master21 <mastercrack2112.5@gmail.com>
2025-09-10 12:20:48 -0600

    Update Home

--- MasterDev/src/Views/Home/Home.jsx
+++ MasterDev/src/Views/Home/Home.jsx
@@ -1,34 +1,41 @@
 import React, { useEffect } from "react";
-
-import "./Home.css";
-
 import { useTranslation } from "react-i18next";
 
 import Prism from "../../Backgrounds/Prism";
+import "./Home.css";
 
 const Home = () => {
     const [t, i18n] = useTranslation("global");
 
-    return(
+    return (
         <>
-        <div className="Home-style">
-                    <div style={{ width: '100%', height: '100vh', position: 'relative', overflow: 'hidden' }}>
-  <Prism
-    animationType="hover"
-    timeScale={0.5}
-    height={3.5}
-    baseWidth={5.5}
-    scale={3.6}
-    hueShift={0}
-    colorFrequency={1}
-    noise={0}
-    glow={0.8}
-    />
-    </div>
-    <p>hola</p>
-        </div>
+            <div className="Home-style">
+                <div
+                    style={{
+                        width: "100%",
+                        height: "100vh",
+                        position: "relative",
+                        overflow: "hidden",
+                    }}
+                >
+                    <Prism
+                        animationType="hover"
+                        timeScale={0.5}
+                        height={3.5}
+                        baseWidth={5.5}
+                        scale={3.6}
+                        hueShift={0}
+                        colorFrequency={1}
+                        noise={0}
+                        glow={0.8}
+                    />
+                </div>
+                <section className="Home-content">
+                    <h1> hola a todos </h1>
+                </section>
+            </div>
         </>
-    )
-}
+    );
+};
 
 export default Home;
