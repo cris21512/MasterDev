e8674a20e41c34ed6bee368ee78cee21c300761e
Master21 <mastercrack2112.5@gmail.com>
2025-09-17 12:34:21 -0600

    Finish Home, start to the About

--- MasterDev/src/Views/About/About.jsx
+++ MasterDev/src/Views/About/About.jsx
@@ -5,7 +5,8 @@ import "./About.css";
 const About = () => {
     return(
         <div className="About-style">
-        <h1>Hola a todos!!</h1>
+        <h1>Hola a todos!</h1>
+        <p>Hello this spam has no color</p>
         </div>
     )
 }
--- MasterDev/src/Views/Home/Home.jsx
+++ MasterDev/src/Views/Home/Home.jsx
@@ -53,7 +53,7 @@ const Home = () => {
                         <img src={MasterDev} alt="MasterDev" className="img-home" />
                         <h1 style={{ fontSize: '4rem' }}>MasterDev</h1>
                         <TextType 
-                        text={["Software Engineer & Design", "Ingeniero de Software & Diseñador", "' Lo que imagines, yo lo programo.'", "' Whatever you imagine, I program it.'"]}
+                        text={["Software Engineer, Web developer & Design", "Ingeniero de Software, Desarrollador Web & Diseñador", "' Lo que imagines, yo lo programo.'", "' Whatever you imagine, I program it.'"]}
                         typingSpeed={75}
                         pauseDuration={1500}
                         showCursor={true}
