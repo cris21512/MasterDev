6e20aa75163b6df14e1961ff1c7b81a0398e8e71
Master21 <mastercrack2112.5@gmail.com>
2025-09-22 09:38:32 -0600

    End About

--- MasterDev/src/Views/About/About.jsx
+++ MasterDev/src/Views/About/About.jsx
@@ -1,13 +1,37 @@
 import React from "react";
-
 import "./About.css";
 
+import Master from '../../assets/Images/Logos/Master.webp'
+
+import ScrollFloat from "../../Components/ScrollFloat/ScrollFloat";
+
 const About = () => {
-    return(
+    return (
         <div className="About-style">
-        <h1>Hola a todos!</h1>
+            <div className="Center-about">
+                <div className="info-style">
+                    <ScrollFloat
+                    animationDuration={1}
+                    ease='back.inOut(2)'
+                    scrollStart="center bottom+=50%"
+                    scrollEnd="bottom bottom-=40%"
+                    stagger={0.03}
+                    >
+                    About me
+                    </ScrollFloat>
+                    <p>
+                        I'm 18 years old and a software developer specialized in frontend web development. I discovered programming in 2023 through a short Kotlin tutorial, but it was in 2024 when I truly began my journey into building web applications. Since then, I've gained experience through personal projects and by collaborating with companies based in the United States including:
+                        <br />• Crazydev
+                        <br />• WYK Homes
+                        <br />• Otis-Valen
+                        <br />
+                        I'm currently focused on improving my skills, expanding my knowledge, and gaining more hands-on experience in the tech industry.💻 I'm MasterDev… and this is my Website!
+                    </p>
+                </div>
+                <img src={Master} alt="Master" style={{ width: '350px', height: 'auto', borderRadius: '1rem' }} />
+            </div>
         </div>
-    )
-}
+    );
+};
 
 export default About;
