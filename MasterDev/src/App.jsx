f2079823f26f18d5bedef38a67541a26864ffe21
Master21 <mastercrack2112.5@gmail.com>
2025-09-22 12:59:16 -0600

    Start to section: 'Projects'

--- MasterDev/src/App.jsx
+++ MasterDev/src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 import Home from './Views/Home/Home'
 import About from './Views/About/About'
 import Tools from './Views/Tools/Tools'
+import Projects from './Views/Projects/Projects'
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
       <Home />
       <About />
       <Tools />
+      <Projects />
     </>
   )
 }
---
+++ MasterDev/src/Views/Projects/Projects.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './Projects.css'
+
+const Projects = () => {
+    return(
+        <div className="Projects-style">
+            <h1>hello guys</h1>
+        </div>
+    )
+}
+
+export default Projects;
