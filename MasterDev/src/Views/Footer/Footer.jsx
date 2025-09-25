2def222c672b6e62b3e0dafe013dacc9f031648b
Master21 <mastercrack2112.5@gmail.com>
2025-09-25 09:51:31 -0600

    Footer section Complete

--- MasterDev/src/Views/Footer/Footer.jsx
+++ MasterDev/src/Views/Footer/Footer.jsx
@@ -4,6 +4,23 @@ import './Footer.css'
 const Footer = () => {
     return(
         <div className="Footer-style">
+            <h1 style={{ fontSize: '5rem' }}>MasterDev - DesignDev</h1>
+            <div className="footer-container">
+                <div className="footer-section">
+                    <h3>Ubicacion</h3>
+                    <span>Guatemala</span>
+                </div>
+                <div className="footer-section">
+                    <h1>Contact</h1>
+                    <h3><a href="mailto:masterkingdev215@gmail.com">masterkingdev215@gmail.com</a></h3>
+                    <p>(+502) 3955-9554</p>
+                </div>
+                <div className="footer-section">
+                    <h3>Redes Sociales</h3>
+                    <p><a href="" >Instagram</a></p>
+                    <p><a href="" >Facebook</a></p>
+                </div>
+            </div>
             <p>Made with ❤️ by MasterDev</p>
         </div>
     )
