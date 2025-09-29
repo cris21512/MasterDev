240ca6ca43fb4a76bb489da1b673212bf7624c3a
Master21 <mastercrack2112.5@gmail.com>
2025-09-29 13:00:24 -0600

    Add component Copy in text

--- MasterDev/src/Views/About/About.jsx
+++ MasterDev/src/Views/About/About.jsx
@@ -5,6 +5,8 @@ import Master from '../../assets/Images/Logos/Master.webp'
 
 import ScrollFloat from "../../Components/ScrollFloat/ScrollFloat";
 
+import Copy from '../../Components/Copy/Copy'
+
 const About = () => {
     return (
         <div className="About-style">
@@ -19,6 +21,7 @@ const About = () => {
                     >
                     About me
                     </ScrollFloat>
+                    <Copy>
                     <p>
                         I'm 18 years old and a software developer specialized in frontend web development. I discovered programming in 2023 through a short Kotlin tutorial, but it was in 2024 when I truly began my journey into building web applications. Since then, I've gained experience through personal projects and by collaborating with companies based in the United States including:
                         <br />• Crazydev
@@ -27,6 +30,7 @@ const About = () => {
                         <br />
                         I'm currently focused on improving my skills, expanding my knowledge, and gaining more hands-on experience in the tech industry.💻 I'm MasterDev… and this is my Website!
                     </p>
+                    </Copy>
                 </div>
                 <img src={Master} alt="Master" style={{ width: '350px', height: 'auto', borderRadius: '1rem' }} />
             </div>
--- MasterDev/src/Views/Projects/Projects.jsx
+++ MasterDev/src/Views/Projects/Projects.jsx
@@ -9,6 +9,8 @@ import Button from '../../Components/Buttons/ButtonsProject/Button'
 import Crazydev from '../../assets/Images/Projects/crazydev.webp';
 import Otis from '../../assets/Images/Projects/Otis.webp';
 
+import Copy from '../../Components/Copy/Copy'
+
 const Projects = () => {
     return (
         <div className="Projects-style">
@@ -44,9 +46,11 @@ const Projects = () => {
                     <div className="box-content">
                         <img src={Crazydev} alt="Crazydev" className="img-project" />
                         <h1 className="name-project" >CrazyDev</h1>
-                        <p style={{ margin: '6px 0 0', opacity: 0.8}}>
-                            A personal website that embodies my approach to modern web development and user-centered design. Built as an elegant digital business card, it features a clean structure, smooth interactions, and a strong focus on visual aesthetics. The site stands out for its intuitive navigation, responsive design, legible typography, and a professional color palette. In the projects section, each work is presented with careful attention to both functionality and visual detail—reinforcing the idea that code can also be art.
-                        </p>
+                        <Copy>
+                            <p style={{ margin: '6px 0 0', opacity: 0.8}}>
+                                A personal website that embodies my approach to modern web development and user-centered design. Built as an elegant digital business card, it features a clean structure, smooth interactions, and a strong focus on visual aesthetics. The site stands out for its intuitive navigation, responsive design, legible typography, and a professional color palette. In the projects section, each work is presented with careful attention to both functionality and visual detail—reinforcing the idea that code can also be art.
+                            </p>
+                        </Copy>
                         <div className="buttons-style">
                             <a href="https://crazydev-three.vercel.app/" target='_blank' rel='noopener noreferrer' ><Button text="View Project" /></a>
                             <a href="https://github.com/cris21512/Crazydev" target='_blank' rel='noopener noreferrer' ><Button text="View on Github" /></a>
@@ -75,9 +79,11 @@ const Projects = () => {
                     <div className="box-content">
                         <img src={Otis} alt="Crazydev" className="img-project" />
                         <h1 className="name-project" >Otis Valen</h1>
-                        <p style={{ margin: '6px 0 0', opacity: 0.8 }}>
-                            A dynamic website that highlights my ability to bring ideas to life through clean code and engaging user experiences. My role focused on frontend development, animations, SEO optimization, and ensuring the site feels both interactive and enjoyable to explore. The design was a collaborative effort with a friend, and together we built a project that is both visually striking and impactful. The site demonstrates how thoughtful development and teamwork can transform ideas.
-                        </p>
+                        <Copy>
+                            <p style={{ margin: '6px 0 0', opacity: 0.8 }}>
+                                A dynamic website that highlights my ability to bring ideas to life through clean code and engaging user experiences. My role focused on frontend development, animations, SEO optimization, and ensuring the site feels both interactive and enjoyable to explore. The design was a collaborative effort with a friend, and together we built a project that is both visually striking and impactful. The site demonstrates how thoughtful development and teamwork can transform ideas.
+                            </p>
+                        </Copy>
                         <div className="buttons-style">
                             <a href="https://otis-valen-nine.vercel.app/" target='_blank' rel='noopener noreferrer' ><Button text="View Project" /></a>
                             <a href="https://github.com/cris21512/Otis-Valen" target='_blank' rel='noopener noreferrer' ><Button text="View on Github" /></a>
