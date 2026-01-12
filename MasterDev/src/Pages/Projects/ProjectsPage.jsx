90122e2f50c0466c94ee07c943d59b38742ff431
Master21 <mastercrack2112.5@gmail.com>
2026-01-12 10:29:59 -0600

    Add and modified ProjectsPage section

--- MasterDev/src/Pages/Projects/ProjectsPage.jsx
+++ MasterDev/src/Pages/Projects/ProjectsPage.jsx
@@ -10,9 +10,12 @@ import Gym from '../../assets/Images/Projects/Gym.webp';
 import Sky from '../../assets/Images/Projects/Sky.webp';
 import CYC from '../../assets/Images/Projects/chitoycris.png';
 import WYK from '../../assets/Images/Projects/house.webp';
+import DES from '../../assets/Images/Projects/Desing.webp';
+import CAS from '../../assets/Images/Projects/Casas.webp';
 
 import Copy from '../../Components/Copy/Copy'
 import Button from '../../Components/Buttons/ButtonsProject/Button'
+import FuzzyText from '../../Components/Buttons/404/FuzzyText';
 
 const ProjectsPage = () => {
 
@@ -50,11 +53,14 @@ const ProjectsPage = () => {
                             </p>
                         </Copy>
                         <div className="buttons-style">
-                            <a href="https://crazydev-three.vercel.app/" target='_blank' rel='noopener noreferrer' ><Button text={t("Tools.View")} /></a>
-                            <a href="https://github.com/cris21512/Crazydev" target='_blank' rel='noopener noreferrer' ><Button text={t("Tools.ViewGit")} /></a>
+                            <FuzzyText
+                            baseIntensity={0.1}
+                            >
+                                not available
+                            </FuzzyText>
                         </div>
                     </div>
-                </ElectricBorderWrapper>             
+                </ElectricBorderWrapper>          
                 <ElectricBorderWrapper
                     color="#3b73af"
                     speed={1}
@@ -160,8 +166,78 @@ const ProjectsPage = () => {
                 </div>
 
                 <div className="projects-row">
-                    <div className="project-card">Proyecto 5</div>
-                    <div className="project-card">Proyecto 6</div>
+                <ElectricBorderWrapper
+                    color="#3b73af"
+                    speed={1}
+                    chaos={0.5}
+                    thickness={2}
+                    style={{ 
+                        borderRadius: 16,
+                        width: '90%',
+                        maxWidth: '500px',
+                        padding: '10px',
+                        marginTop: '40px',
+                        height: 'max-content',
+                        minHeight: '400px',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        display: 'flex',
+                        flexDirection: 'column',
+                    }}
+                >
+                    <div className="box-content">
+                        <img src={DES} alt="Desing-Dev" className="img-project" />
+                        <h1 className="name-project" >Design-Dev</h1>
+                        <Copy>
+                            <p style={{ margin: '6px 0 0', opacity: 0.8}}>
+                                {t("Tools.Project3")}
+                            </p>
+                        </Copy>
+                        <div className="buttons-style">
+                            <FuzzyText
+                            baseIntensity={0.1}
+                            >
+                                not available
+                            </FuzzyText>
+                        </div>
+                    </div>
+                </ElectricBorderWrapper>
+                <ElectricBorderWrapper
+                    color="#3b73af"
+                    speed={1}
+                    chaos={0.5}
+                    thickness={2}
+                    style={{ 
+                        borderRadius: 16,
+                        width: '90%',
+                        maxWidth: '500px',
+                        padding: '10px',
+                        marginTop: '40px',
+                        height: 'max-content',
+                        minHeight: '400px',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        display: 'flex',
+                        flexDirection: 'column',
+                    }}
+                >
+                    <div className="box-content">
+                        <img src={CAS} alt="CASAS" className="img-project" />
+                        <h1 className="name-project" >CASAS</h1>
+                        <Copy>
+                            <p style={{ margin: '6px 0 0', opacity: 0.8}}>
+                                {t("Tools.Project3")}
+                            </p>
+                        </Copy>
+                        <div className="buttons-style">
+                            <FuzzyText
+                            baseIntensity={0.1}
+                            >
+                                not available
+                            </FuzzyText>
+                        </div>
+                    </div>
+                </ElectricBorderWrapper>
                 </div>
             </section>
         </div>
